77e993570e40435134bba19b0f8bd31b8b88b6f0
Raul Sanchez R. <raul.sr92@gmail.com>
2025-10-21 19:10:37 -0500

    Clase 4: se crearon 2 endpoint adicionales: de busqueda por ID y con parametros query

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 
 import express from "express"
 import dotenv from "dotenv"
+import bodyParser from "body-parser"
 
 const app = express()
 
 //Cargar variables de entorno
-
 dotenv.config()
-
 
 const port = process.env.PORT || 3005;
 console.log(port)
+
+//Middleware para parsear JSON
+
+/*app.use(express.json())*/
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 
 
 
@@ -22,13 +28,51 @@ app.get('/', (req, res) => {
     <h1>Bienvenidos</h1>
     <p>Hola desde HTML</p>
     <p>Sea usted Bienvenido</p>
-
     <a href="">Ingresar</a>
-
-    
     `
   )
 })
+
+// Segundo Enpoint
+
+app.post('/eventos',(req,res)=>{
+
+  const obj1 = req.body
+  res.send(obj1)
+  console.log(obj1)
+
+})
+
+//Tercer endpoint
+
+app.get('/users/:id',(req, res)=>{
+
+  const userId = req.params.id
+
+  console.log(userId)
+
+  res.send(`Mostrar información del usuario con ID ${userId}`)
+
+})
+
+//Cuarto endpoint
+
+app.get('/search',(req,res)=>{
+
+  const terms = req.query.termino || "No especificado"
+
+  const category = req.query.categoria || "Todas"
+
+  res.send(
+    `
+    <h2>Resultados de búsqueda:</h2>
+    <p>Término: ${terms}</p>
+    <p>Categoría: ${category}</p>
+
+    `
+  )
+})
+
 
 
 //Inicializar el servidor
