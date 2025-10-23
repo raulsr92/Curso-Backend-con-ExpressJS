09c6b4cc6bfa99b9241b476fcd0d41effc3e8262
Raul Sanchez R. <raul.sr92@gmail.com>
2025-10-23 14:10:22 -0500

    Clase 5: creacion de 2 rutas para procesar formularios y objeto JSON

--- app.js
+++ app.js
@@ -13,11 +13,14 @@ console.log(port)
 
 //Middleware para parsear JSON
 
-/*app.use(express.json())*/
-
+//>>> Con body parser 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
+//>>> Con versiones modernas de express
+
+/*app.use(express.json())*/
+/*app.use(expressurlencoded({extended:true}))*/
 
 
 // Primer endpoint
@@ -72,6 +75,58 @@ app.get('/search',(req,res)=>{
     `
   )
 })
+
+//Quinto endpoint - formularios
+
+app.post('/form',(req,res)=>{
+
+  //Accediendo a la propiedad nombre del cuerpo
+
+  const name = req.body.nombre ||'Anónimo'
+  const email = req.body.email ||'No proporcionado'
+
+  //Respuesta
+
+  res.json(
+    {
+      message: 'Datos recibidos',
+      data:
+        {
+          name,
+          email
+        }
+    }
+  )
+}
+)
+
+//Sexto endpoint - objeto JSON
+
+app.post('/api/data', (req, res)=>{
+
+  const data = req.body 
+
+  //Validación de data
+
+  if(!data || Object.keys(data).length ===0 ){
+    return res.status(400).json({error: "No se recibieron datos"})
+  }
+
+  //Respuesta
+
+  res.status(201).json(
+    {
+      message: "Datos JSON recibidos",
+      data
+    }
+
+  )
+
+}
+
+)
+
+
 
 
 
